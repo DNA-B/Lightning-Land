e7f64c9280e7f259e642408976ae17638be64e7e
변성훈 <sphz3813@gmail.com>
2023-12-09 00:34:05 +0900

    Update data_init.h

--- header/data_init.h
+++ header/data_init.h
@@ -1,9 +1,15 @@
+ #ifndef _DATA_INIT_H
+ #define _DATA_INIT_H
+
 struct Data {
 	char name[20];
+	int idx;
 	int price;
 	int min_price;
 	int is_selled;
-	int is_trade;
+	int is_trading;
+	pid_t cur_ppid;
 };
 
 struct Data* data_init();
+ #endif
