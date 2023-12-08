cffe2f63c34f98c3de0873adaf4bfd1015b85728
변성훈 <sphz3813@gmail.com>
2023-12-09 00:34:52 +0900

    Update client.c

--- source/client.c
+++ source/client.c
@@ -7,13 +7,25 @@
 
 #define SOCKET_NAME "trade"
 
-extern int initsem(key_t semkey);
-extern int semlock(int semid);
-extern int semunlock(int semid);
+char buf[BUFSIZ];
+
+void send_msg(int sd, char* msg) {		
+	strcpy(buf, msg);
+	if(send(sd, buf, strlen(buf) + 1, 0) == -1) {
+		perror("send");
+		exit(1);
+	}	
+}
+
+int receive_msg(int sd) {
+	if(recv(sd, buf, sizeof(buf), 0) == -1) {
+		perror("recv");
+		exit(1);
+	}
+}
 
 int main() {
-	int sd, len, semid;
-	char buf[BUFSIZ];
+	int sd, len;
 	struct sockaddr_un ser;
 
 	if ((sd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
@@ -30,26 +42,23 @@ int main() {
 		perror("bind");
 		exit(1);
 	}
-
-
-	if (recv(sd, buf, sizeof(buf), 0) == -1) {
-		perror("recv");
-		exit(1);
-	}
 	
-	printf("%s", buf);
-	
-	if(strcmp(buf, "번개나라에 오신 것을 환영합니다.\n구매하실 물건을 선택해주세요.")) {
-		printf("Select item : ");
-		scanf("%s", buf);
-	}
-	
-	if (send(sd, buf, sizeof(buf), 0) == -1) {
-		perror("send");
-		exit(1);
+	while(1) {
+		receive_msg(sd);
+		
+		if(strcmp(buf, "1") == 0) {
+			printf("Select item : ");
+			scanf("%s", buf);
+			send_msg(sd, buf);
+		}
+		else if(strcmp(buf, "2") == 0) {
+			break;
+		}
+		else {
+			printf("%s\n", buf);
+		}
 	}
 	
 	close(sd);
 }
-
 
