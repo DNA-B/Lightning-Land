d2507c7539e4656d1aa50a53cde3eb51dee2e13e
변성훈 <sphz3813@gmail.com>
2023-12-10 18:55:24 +0900

    Create ready.h

---
+++ header/ready.h
@@ -0,0 +1,10 @@
+ #ifndef _READY_H
+ #define _READY_H
+ 
+void send_msg(int nsd, char* msg);
+char* receive_msg(int nsd);
+void select_item(int nsd);
+int re_trade(int nsd);
+void trade_or_exit(int nsd);
+
+#endif
