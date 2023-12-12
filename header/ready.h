6733c2ae923406f91779400441c11b6e960947a1
변성훈 <sphz3813@gmail.com>
2023-12-12 21:08:39 +0900

    Update ready.h

--- header/ready.h
+++ header/ready.h
@@ -5,7 +5,7 @@ int item_idx;
 char buf[BUFSIZ];
 
 void send_msg(int nsd, char* msg);
-char* receive_msg(int nsd);
+void receive_msg(int nsd);
 void select_item(int nsd);
 int re_trade(int nsd);
 void trade_or_exit(int nsd);
