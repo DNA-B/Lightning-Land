c94d078d3040ed69c018938b87eb6c1bbc81f54f
변성훈 <sphz3813@gmail.com>
2023-12-13 17:18:46 +0900

    Update data_save.c

--- source/data_save.c
+++ source/data_save.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "data_init.h"
 #include <unistd.h>
+#include "data_init.h"
 
-int main() {
-
-	FILE *fp = fopen("LL_data.txt", "w");
+void data_save(Data* item) {
+	FILE *fp = fopen("LL_after_data.txt", "w");
 
 	if (fp == NULL) {
 		perror("open error");
-		exit(1)
+		exit(1);
 	}
 
-	for (i = 0; i < 7; ++i) {
-		fprintf(fp, "%s %d %d %d %d\n",
+	for (int i = 0; i < 7; ++i) {
+		fprintf(fp, "%s %s %s %d %d\n",
 				item[i].name,
-				item[i].idx,
 				item[i].price,
 				item[i].min_price,
+				item[i].is_selled,
 				item[i].is_trading);
 	}
 
 	fclose(fp);
-
-	return 0;
 }
-
